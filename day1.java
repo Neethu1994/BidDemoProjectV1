76866c48d46615f2be0344fa5252fbc9ad8c2c7e
Neethu <Neethu@DESKTOP-OETEOAA>
2024-01-19 21:32:27 +0530

    First commit

---
+++ day1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.ict.day1;
+
+/**
+ * 
+ */
+public class day1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int a=10;
+		System.out.println("stored value is "+ a);
+		boolean flag= true;
+		System.out.println("Boolen value stored is "+flag);
+		// TODO Auto-generated method stub
+
+	}
+
+}
